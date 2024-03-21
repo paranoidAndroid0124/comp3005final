8b40e40d6d97cc22d82ba8987fc3339f5d85d622
Vincent Gagnon <vgagnon@zeptile.com>
2024-03-20 21:20:47 -0400

    started to add routes

--- index.ts
+++ index.ts
@@ -4,8 +4,9 @@ import { migrate } from 'drizzle-orm/postgres-js/migrator';
 import postgres from 'postgres';
 import { promisify } from "node:util";
 import { createInterface} from "node:readline";
+import Fastify from 'fastify';
 
-import { student } from './src/drizzle/schema';
+import { membersRoutes } from './routes/membersRoutes';
 import {eq} from "drizzle-orm";
 
 const migrationConnection = postgres(process.env.DATABASE_URL!, { max: 1 });
@@ -22,21 +23,47 @@ const  questionAsync = promisify(readline.question).bind(readline);
 
 const main = async () => {
     // *** Set up db *** //
-    await migrate(drizzle(migrationConnection), { migrationsFolder: 'src/drizzle' });
-    await migrationConnection.end();
+    // await setUpDB();
 
     // *** insert data *** //
     // TODO: can probably do this in schema
-    await insertInitialData();
+    //await insertInitialData();
 
-    process.exit(0);
+    // *** Set up and start Fastify server *** //
+    const fastify = Fastify({ logger: true});
+
+    // Declare a route
+    fastify.get('/', async (request, reply) => {
+        return { hello: 'world' };
+    });
+
+    fastify.register(membersRoutes);
+
+    console.log('Starting server');
+    // Run the server!
+    try {
+        await fastify.listen({ port: 3000 });
+        const address = fastify.server.address();
+        const port = typeof address === 'string' ? address : address?.port;
+
+        console.log('server listening on ${port}');
+        fastify.log.info(`server listening on ${port}`);
+    } catch (err) {
+        fastify.log.error(err);
+        process.exit(1);
+    }
 };
 
+async function setUpDB():Promise<void> {
+    await migrate(drizzle(migrationConnection), { migrationsFolder: 'src/drizzle' });
+    await migrationConnection.end();
 
-async function insertInitialData():Promise<void> {
-    await db.insert(student).values({first_name: 'john', last_name: 'Doe', email: 'john.doe@example.com', enrollment_date: '2023-09-01'}).execute();
-    await db.insert(student).values({first_name: 'Jane', last_name: 'Smith', email: 'jane.smith@example.com', enrollment_date: '2023-09-01'}).execute();
-    await db.insert(student).values({first_name: 'Jim', last_name: 'Beam', email: 'jim.beam@example.com', enrollment_date: '2023-09-02'}).execute();
 }
 
-main();
+async function insertInitialData():Promise<void> {
+    // await db.insert(student).values({first_name: 'john', last_name: 'Doe', email: 'john.doe@example.com', enrollment_date: '2023-09-01'}).execute();
+    // await db.insert(student).values({first_name: 'Jane', last_name: 'Smith', email: 'jane.smith@example.com', enrollment_date: '2023-09-01'}).execute();
+    // await db.insert(student).values({first_name: 'Jim', last_name: 'Beam', email: 'jim.beam@example.com', enrollment_date: '2023-09-02'}).execute();
+}
+
+main().catch(err => console.error(err));
--- src/drizzle/schema.ts
+++ src/drizzle/schema.ts
@@ -1,9 +1,71 @@
 import {date, integer, pgTable, serial, text, pgSchema} from 'drizzle-orm/pg-core';
 
-export const student = pgTable('student', {
-    student_id: serial('id').primaryKey(),
+export const billingInformation = pgTable('billingInformation', {
+    billing_id: serial('billing_id').primaryKey(),
+    periodicity: text('periodicity').notNull(),
+    payment_info: text('paymentInfo').notNull(), // TODO: this will be a table
+    card_type: text('cardType'),
+    card_holder: text('cardHolder').notNull(),
+    card_number: text('cardNumber').notNull(),
+    expiry: date('expiry').notNull()
+});
+
+export const members = pgTable('member', {
+    member_id: serial('member_id').primaryKey(),
+    billing_id: integer('billing_id').references(() => billingInformation.billing_id),
     first_name: text('first_name').notNull(),
     last_name: text('last_name').notNull(),
-    email: text('email').notNull().unique(),
-    enrollment_date: date('enrollment_date')
-});
+    address: text('address'),
+    health_metric: text('health_metrix'),
+    fitness_goals: text('fitness_goals'),
+    fitness_achivements: text('fitness_achivements'),
+    join_date: date('join_date'),
+    email: text('email').unique(),
+    password: text('password'),
+    phone_number: text('phone_number')
+})
+
+export const paymentInfo = pgTable ('paymentInfo', {
+    payment_id: serial('payment_id').primaryKey(),
+    member_id: integer('member_id').references(()=> members.member_id),
+    payment_date: date(`payment_date`),
+    amount: integer('amount')
+})
+
+export const membershipCard = pgTable ('membershipCard', {
+    member_id: integer('member_id').references(()=> members.member_id),
+    nfc: text('nfc')
+})
+
+export const equipment = pgTable ('equipment', {
+    equipment_id: serial('equipment_id').primaryKey(),
+    equipment_name: text('equipment_name'),
+    last_maintained: date('last_maintained').notNull(),
+    next_maintained: date('next_maintained').notNull()
+})
+
+export const room = pgTable ('room', {
+    room_id: serial('room_id').primaryKey(),
+    train_id: integer('trainer_Id'),
+    capacity: integer('capacity')
+})
+
+export const trainer = pgTable ( 'trainer', {
+    trainer_id: integer('trainer_id').primaryKey(),
+    name: text('name'),
+    address: text('address'),
+    phone_number: text('phone_number')
+})
+
+export const exercise = pgTable ('exercise',{
+    exercise_type: text('exercise').primaryKey(),
+    reps: integer('reps'),
+    duration: integer('duration')
+})
+
+export const adminStaff = pgTable ('adminStaff', {
+    staff_id: serial('staff_id'),
+    first_name: text('first_name').notNull(),
+    last_name: text('last_name').notNull(),
+    email: text('email').notNull().unique()
+})
